06f53c3d135875a1ffcc856fa3ade015709929c4
timotiousPrime <timodb031@gmail.com>
2021-05-25 22:57:34 +0200

    answered some questions

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
 
+    people.reduce( (acc, person) => {
+
+        let age = () => {
+            if (person.yearOfDeath !== undefined) {
+                return person.yearOfDeath - person.yearOfBirth
+            }else {
+                return (new Date()).getFullYear() - person.yearOfBirth
+            }};
+        if (acc === null || acc < age) return person.name;
+        return acc.name
+    }, null)
 }
 
 module.exports = findTheOldest
