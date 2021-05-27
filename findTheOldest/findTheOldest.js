5f119ca36328e6c9dad76e71704fc869adbc7b8e
Timo <timodb031@gmail.com>
2021-05-27 11:49:12 +0200

    Completed find the oldest tests

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,16 +1,25 @@
 let findTheOldest = function(people) {
-
-    people.reduce( (acc, person) => {
-
-        let age = () => {
-            if (person.yearOfDeath !== undefined) {
-                return person.yearOfDeath - person.yearOfBirth
-            }else {
-                return (new Date()).getFullYear() - person.yearOfBirth
-            }};
-        if (acc === null || acc < age) return person.name;
-        return acc.name
-    }, null)
+    console.table(people)
+    let oldest = people.reduce( (acc, person) => {
+        function age(obj) {
+            if (obj.yearOfDeath === undefined) {
+                return (new Date()).getFullYear() - obj.yearOfBirth;
+            } else {
+                return obj.yearOfDeath - obj.yearOfBirth
+            }
+        }
+        if (acc === null || age(acc) < age(person)) {
+            console.log(`${acc.name} ${age(acc)}`)
+            console.log(`${person.name} ${age(person)}`)
+            return person}
+        else {
+            console.log(`${acc.name} ${age(acc)}`)
+            console.log(`${person.name} ${age(person)}`)            
+            return acc}
+    })
+    console.log(oldest)
+    return oldest
 }
 
 module.exports = findTheOldest
+
