ee0a23b3ce9350b47a914859038fefa4e5952e55
Timo <timodb031@gmail.com>
2021-05-27 10:25:56 +0200

    Completed palindromes test

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    let newStr = '';
+    let revStr = '';
+    let letter = /[a-z]/i
+    for (let i = 0; i < string.length; i++) {
+        if (letter.test(string[i])) {
+            newStr += string[i]
+        }
+    }
+    for (let j = newStr.length; j > 0; j--) {
+        revStr += newStr[j-1]
+    }
+    return newStr.toLowerCase() === revStr.toLowerCase()
 }
 
 module.exports = palindromes
