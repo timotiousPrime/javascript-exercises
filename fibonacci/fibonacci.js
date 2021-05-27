0a7741cd796735d7b2f23b518ee57fa983ac21fd
Timo <timodb031@gmail.com>
2021-05-27 13:02:58 +0200

    Completed fibonacci sequence test

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    x = Number(x);
 
+    if (x < 1) return 'OOPS';
+
+    let sequence = [1, 1];
+
+    for (let i = 1; i <= x-2; i++) {
+        let y = sequence[i-1] + sequence[i]
+        sequence.push(y);
+    };
+    console.log(sequence[x-1]);
+    return sequence[x-1];
 }
-
 module.exports = fibonacci
